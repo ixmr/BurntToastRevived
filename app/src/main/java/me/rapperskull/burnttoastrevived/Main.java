ddc381f4f88c660b6ba3e3e700ef996ca5433b70
ixmr <opixmr@proton.me>
2024-07-05 15:00:22 +0200

    some playing around

--- app/src/main/java/me/rapperskull/burnttoastrevived/Main.java
+++ app/src/main/java/me/rapperskull/burnttoastrevived/Main.java
@@ -1,52 +1,46 @@
 package me.rapperskull.burnttoastrevived;
 
-import android.os.Build;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.content.res.XResources;
-import android.view.Gravity;
-import android.widget.GridLayout.LayoutParams;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import java.io.File;
 
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.callbacks.XC_LayoutInflated;
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.XResources;
 
 public class Main implements IXposedHookZygoteInit {
     private XSharedPreferences pref;
     private int size;
     private int margin;
     private Context context;
-    private int LateralMargin=0;
-    private int VerticalMargin=0;
-    private int CentralMargin=0;
-    final boolean isN=(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.N);
-    private static final File prefsFile = new File("/data/user_de/0/me.rapperskull.burnttoastrevived/shared_prefs/settings.xml");
+    private int LateralMargin = 0;
+    private int VerticalMargin = 0;
+    private int CentralMargin = 0;
+    private boolean isN = (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N);
 
     @Override
-    public void initZygote(StartupParam startupParam) throws Throwable {
+    public void initZygote(IXposedHookZygoteInit.StartupParam startupParam) throws Throwable {
         XC_LayoutInflated hook = new XC_LayoutInflated() {
             @Override
-            public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
-
+            public void handleLayoutInflated(XC_LayoutInflated.LayoutInflatedParam liparam) throws Throwable {
                 commonScripts();
 
-                if(liparam.view instanceof LinearLayout){
+                if (liparam.view instanceof LinearLayout) {
                     XposedBridge.log("Burnt Toast Revived: Found LinearLayout");
 
                     LinearLayout layout = (LinearLayout) liparam.view;
                     context = layout.getContext();
-                    TextView view = (TextView) liparam.view.findViewById(android.R.id.message);
+                    TextView view = (TextView) layout.findViewById(android.R.id.message);
 
                     LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                            LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-                    params.gravity = Gravity.CENTER;
+                            LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    params.gravity = android.view.Gravity.CENTER;
                     params.rightMargin = LateralMargin;
 
                     view.setLayoutParams(params);
@@ -54,55 +48,55 @@ public class Main implements IXposedHookZygoteInit {
                     PackageManager pm = context.getPackageManager();
 
                     ImageView imageView = new ImageView(context);
-                    int imageViewSize = Math.max(view.getHeight(),size);
+                    int imageViewSize = Math.max(view.getHeight(), size);
                     imageView.setMaxHeight(imageViewSize);
                     imageView.setMaxWidth(imageViewSize);
                     imageView.setAdjustViewBounds(true);
                     imageView.setImageDrawable(context.getApplicationInfo().loadIcon(pm));
 
                     LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(
-                            LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-                    params1.gravity = Gravity.CENTER;
-                    params1.setMargins(LateralMargin,VerticalMargin,CentralMargin,VerticalMargin);
+                            LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    params1.gravity = android.view.Gravity.CENTER;
+                    params1.setMargins(LateralMargin, VerticalMargin, CentralMargin, VerticalMargin);
                     imageView.setLayoutParams(params1);
 
                     layout.setOrientation(LinearLayout.HORIZONTAL);
                     layout.addView(imageView, 0);
 
-                } else if(liparam.view instanceof RelativeLayout){
+                } else if (liparam.view instanceof RelativeLayout) {
                     XposedBridge.log("Burnt Toast Revived: Found RelativeLayout");
 
                     RelativeLayout layout = (RelativeLayout) liparam.view;
                     context = layout.getContext();
-                    TextView view = (TextView) liparam.view.findViewById(android.R.id.message);
+                    TextView view = (TextView) layout.findViewById(android.R.id.message);
 
                     layout.setBackground(view.getBackground());
 
                     LinearLayout inner = new LinearLayout(context);
                     RelativeLayout.LayoutParams innerParams = new RelativeLayout.LayoutParams(
-                            LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-                    innerParams.addRule(RelativeLayout.CENTER_IN_PARENT , RelativeLayout.TRUE);
-                    innerParams.setMargins(0,0,0,0);
+                            RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                    innerParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+                    innerParams.setMargins(0, 0, 0, 0);
                     inner.setLayoutParams(innerParams);
 
                     PackageManager pm = context.getPackageManager();
 
                     ImageView imageView = new ImageView(context);
-                    int imageViewSize = Math.max(view.getHeight(),size);
+                    int imageViewSize = Math.max(view.getHeight(), size);
                     imageView.setMaxHeight(imageViewSize);
                     imageView.setMaxWidth(imageViewSize);
                     imageView.setAdjustViewBounds(true);
                     imageView.setImageDrawable(context.getApplicationInfo().loadIcon(pm));
 
                     LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(
-                            LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-                    params1.gravity = Gravity.CENTER;
-                    params1.setMargins(LateralMargin,VerticalMargin,CentralMargin,VerticalMargin);
+                            LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    params1.gravity = android.view.Gravity.CENTER;
+                    params1.setMargins(LateralMargin, VerticalMargin, CentralMargin, VerticalMargin);
                     imageView.setLayoutParams(params1);
 
                     LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                            LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-                    params.gravity = Gravity.CENTER;
+                            LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    params.gravity = android.view.Gravity.CENTER;
                     params.rightMargin = LateralMargin;
                     params.leftMargin = 0;
 
@@ -118,7 +112,6 @@ public class Main implements IXposedHookZygoteInit {
                 } else {
                     XposedBridge.log("Burnt Toast Revived: Unknown Layout");
                 }
-
             }
         };
 
@@ -126,14 +119,13 @@ public class Main implements IXposedHookZygoteInit {
         try {
             XResources.hookSystemWideLayout("android", "layout", "tw_transient_notification", hook);
         } catch (Resources.NotFoundException e) {
-
-
+            XposedBridge.log("Burnt Toast Revived: Layout tw_transient_notification not found");
         } catch (Throwable t) {
-
+            XposedBridge.log("Burnt Toast Revived: Error hooking layout tw_transient_notification");
         }
     }
 
-    private void commonScripts(){
+    private void commonScripts() {
         if (isN) {
             pref = new XSharedPreferences(prefsFile);
         } else {
@@ -143,17 +135,15 @@ public class Main implements IXposedHookZygoteInit {
         pref.reload();
         size = pref.getInt("icon_size", 96);
         margin = pref.getInt("margin_size", 1);
-        XposedBridge.log("Burnt Toast Revived: Got size of " + Integer.toString(size) + " and margin of " + Integer.toString(margin));
+        XposedBridge.log("Burnt Toast Revived: Got size of " + size + " and margin of " + margin);
 
-        CentralMargin = 5+5*margin;
-        if(margin==0){
+        CentralMargin = 5 + 5 * margin;
+        if (margin == 0) {
             LateralMargin = 0;
             VerticalMargin = 0;
         } else {
-            LateralMargin = 10*(margin+1);
-            VerticalMargin = 10+5*margin;
+            LateralMargin = 10 * (margin + 1);
+            VerticalMargin = 10 + 5 * margin;
         }
-
     }
-
 }
